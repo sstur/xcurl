6ec6f981de746fba25ee13c1fe6087b9ba21278e
Simon <sstur@me.com>
2021-11-21 17:33:41 -0800

    Initial implementation using fetch()

--- src/cli.ts
+++ src/cli.ts
@@ -1,2 +1,17 @@
-// eslint-disable-next-line no-console
-console.log('Hello world');
+import fetch from 'node-fetch';
+
+async function main() {
+  let args = process.argv.slice(2);
+
+  let url = args[0] || '';
+
+  let response = await fetch(url);
+  let result = await response.text();
+  console.log(result);
+}
+
+main().catch((e) => {
+  // eslint-disable-next-line no-console
+  console.error(e);
+  process.exit(1);
+});
