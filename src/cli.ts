5fe617fd01387f910051c5f13aa2db0812847c84
Simon <sstur@me.com>
2021-11-21 18:00:39 -0800

    Args parsing using mri()

--- src/cli.ts
+++ src/cli.ts
@@ -1,9 +1,43 @@
+/* eslint-disable no-console */
+import { readFile } from 'fs/promises';
+import { join } from 'path';
+
 import fetch from 'node-fetch';
+import mri from 'mri';
+
+let argv = process.argv.slice(2);
+
+let args = mri(argv, {
+  alias: {
+    d: 'data',
+    f: 'fail',
+    h: 'help',
+    i: 'include',
+    o: 'output',
+    O: 'remote-name',
+    s: 'silent',
+    T: 'upload-file',
+    u: 'user',
+    A: 'user-agent',
+    v: 'verbose',
+    V: 'version',
+  },
+  boolean: ['f', 'h', 'i', 'O', 's', 'v', 'V'],
+  string: ['d', 'o', 'T', 'u', 'A'],
+  unknown: (input) => {
+    console.log(`Unknown argument: ${input}`);
+    process.exit(1);
+  },
+});
 
 async function main() {
-  let args = process.argv.slice(2);
+  if (args.help) {
+    let usage = await readFile(join(__dirname, '../usage.txt'), 'utf8');
+    console.log(usage);
+    process.exit(0);
+  }
 
-  let url = args[0] || '';
+  let url = args._[0] || '';
 
   let response = await fetch(url);
   let result = await response.text();
@@ -11,7 +45,6 @@ async function main() {
 }
 
 main().catch((e) => {
-  // eslint-disable-next-line no-console
   console.error(e);
   process.exit(1);
 });
