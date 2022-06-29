62f024fde6eb4cfd11fdf7b762fb175f63729d1a
Simon <sstur@me.com>
2022-06-29 11:57:49 -0700

    Add support for request headers

--- src/support/getFetchOptions.ts
+++ src/support/getFetchOptions.ts
@@ -1,6 +1,6 @@
 import { URL } from 'url';
 
-import { RequestInit } from 'node-fetch';
+import { Headers, RequestInit } from 'node-fetch';
 import { CommandLineOptions } from 'command-line-args';
 
 const methodsWithBody: Record<string, true> = {
@@ -22,18 +22,46 @@ export function getFetchOptions(
   args: CommandLineOptions,
 ): RequestInit {
   let method = toString(args.request, '').toLowerCase();
+  let headersArray = toStringArray(args.header);
+  let headers = new Headers();
+  for (let header of headersArray) {
+    let parsed = parseHeader(header);
+    if (parsed) {
+      let [name, value] = parsed;
+      headers.append(name, value);
+    }
+  }
   let body = toString(args.data, null);
   if (body != null && methodsWithBody[method] !== true) {
     method = 'post';
   } else if (validMethods[method] !== true) {
     method = 'get';
   }
+  if (body != null && !headers.has('Content-Type')) {
+    headers.set('Content-Type', 'application/x-www-form-urlencoded');
+  }
   return {
     method,
+    headers,
     body: body ?? undefined,
   };
 }
 
+function parseHeader(input: string): [string, string] | null {
+  let [name, ...values] = input.split(':');
+  if (name && values.length) {
+    let value = values.join(':');
+    return [name.trim(), value.trim()];
+  }
+  return null;
+}
+
 function toString<T = null>(value: unknown, fallback: T): string | T {
   return typeof value === 'string' ? value : fallback;
+}
+
+function toStringArray(input: unknown): Array<string> {
+  return Array.isArray(input)
+    ? input.filter((el) => typeof el === 'string')
+    : [];
 }
